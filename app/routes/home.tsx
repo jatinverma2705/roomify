0e68c3951fde72b9b02cd2c33008f552963bce7e
Jatin Verma <jatinverma2705@gmail.com>
2026-02-15 15:25:34 +0530

    Implemented Homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,15 +1,102 @@
 import type { Route } from "./+types/home";
 import NavBar from "../../Components/NavBar";
+import { ArrowRight } from "lucide-react";
+import Button from "Components/ui/Button";
+import { Layers } from "lucide-react";
+import { Clock } from "lucide-react";
+import { ArrowUpRight } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Roomify" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <div className="home">
-    <NavBar />
-  </div>
-};
+  return (
+    <div className="home">
+      <NavBar />
+
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introducing Roomify 2.0</p>
+        </div>
+        <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
+        <p className="subtitle">
+          Roomify is an AI-first design environment that empowers you visualize
+          , render and ship architectural projects faster than ever.
+        </p>
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+
+          <Button variant="outline" size="lg" className="demo">
+            Watch Demo
+          </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor Plan</h3>
+              <p>Supports JPG,PNG, formats upto 10MB</p>
+            </div>
+            <p>Upload images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community projects , all in one
+                place.
+              </p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="project"
+                />
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date().toLocaleDateString()}</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUpRight size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
